d4beb78d3a8b8dd8b63974824cbfca4e02517779
Preston Carlton <prescarlton@gmail.com>
2024-06-11 13:38:58 -0400

    adds resume

--- app/resume/page.tsx
+++ app/resume/page.tsx
@@ -1,8 +1,14 @@
+import { CustomMDX } from "app/components/mdx"
+import { getResumeData } from "utils"
+
 export default function Page() {
+  const content = getResumeData()
   return (
     <section>
       <h1 className="mb-8 text-2xl font-semibold tracking-tighter">resume</h1>
-      <p>{`Doesn't look like much yet, but pretty soon you'll see my resume here.`}</p>
+      <article className="prose">
+        <CustomMDX source={content} />
+      </article>
     </section>
   )
 }
