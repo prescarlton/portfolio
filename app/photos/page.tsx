40c754ba404823244f8291c14a8aaeb16e4139ba
Preston Carlton <prescarlton@gmail.com>
2024-11-21 14:20:50 -0500

    adds some pretty pictures

--- app/photos/page.tsx
+++ app/photos/page.tsx
@@ -1,8 +1,29 @@
+import georgia from "./georgia.jpg"
+import nevada from "./nevada.jpg"
+import rocky from "./rocky.jpg"
+import saltFlats from "./salt_flats.jpg"
+import yosemite from "./yosemite.jpg"
+import uhwarrie from "./uhwarrie.jpg"
+import Image from "next/image"
+
 export default function Page() {
+  const imageClassName = "rounded-lg shadow-lg h-64 w-full object-cover"
   return (
     <section>
-      <h1 className="mb-8 text-2xl font-semibold tracking-tighter">photos</h1>
-      <p>{`Doesn't look like much yet, but pretty soon you'll see some of the pictures I've taken.`}</p>
+      <h1 className="text-2xl font-semibold tracking-tighter">photos</h1>
+      <p className="mb-8">
+        just a random assortment of pictures I've taken that I really like.
+        Working on a better way to display these, with more context and better
+        organization.
+      </p>
+      <Image src={nevada} alt="nevada" className={imageClassName + " mb-4"} />
+      <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3">
+        <Image src={georgia} alt="georgia" className={imageClassName} />
+        <Image src={rocky} alt="rocky" className={imageClassName} />
+        <Image src={saltFlats} alt="salt flats" className={imageClassName} />
+        <Image src={yosemite} alt="yosemite" className={imageClassName} />
+        <Image src={uhwarrie} alt="uhwarrie" className={imageClassName} />
+      </div>
     </section>
   )
 }
